0fcea64c573450969406bd5da69e6a53aa095967
Muhammad Saqib <sm4179515@gmail.com>
2024-10-20 10:54:58 +0500

    now update the delete user

--- server.mjs
+++ server.mjs
@@ -90,7 +90,7 @@ app.put('/users/:id', async (req, res) => {
 app.delete('/users/:id', async (req, res) => {
     try {
         const user = await User.findByIdAndDelete(req.params.id);
-        if (!user) return res.status(4).json({ message: 'User not found' });
+        if (!user) return res.status(404).json({ message: 'User not found' });
         res.status(200).json({ message: 'User deleted' });
     } catch (error) {
         res.status(500).json({ message: error.message });
